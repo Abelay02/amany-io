41ad135e81952e33236c62b0ebed0a7621109bea
Abelay02 <belay.a.amanuel@gmail.com>
2023-12-12 22:06:45 -0700

    fix server routing issue

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
 
 const app = express();
 
+// Handle SPA routing
+app.use(history());
+
 // Serve the static files from the Vue.js app
-app.use(serveStatic(path.join(__dirname, '/dist')));
+app.use(serveStatic(path.join(__dirname, "/dist")));
 
 // Set port
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('Server started on port ' + port);
+console.log("Server started on port " + port);
